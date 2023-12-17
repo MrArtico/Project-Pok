45f934111692918e67305df8a90af27a0887d07b
Thiago <thiago.goncalvesgames@gmail.com>
2023-12-17 17:29:01 -0300

    Modificações gerais

--- script.js
+++ script.js
@@ -1,36 +1,35 @@
 const pokemonButton = document.getElementById('addPokemon');
-const pokedexLista = document.getElementById('gridPok');
+const pokedexLista = document.getElementById('listagem');
 const form = document.querySelector('form');
-const radios = document.querySelectorAll('input.tipos');
+const buttonSubmit = document.getElementById('sendPokemon');
+const nome = document.getElementById('nomePokemon');
+const numeroPok = document.getElementById('numberPokemon');
+const url = document.getElementById('imgPokemon');
 
 pokemonButton.addEventListener('click', () => {
     form.setAttribute('class', 'criacaoPokemon');
     pokemonButton.style.display = 'none';
-    const buttonSubmit = document.createElement('input');
-    buttonSubmit.setAttribute('type', 'submit');
-    buttonSubmit.setAttribute('id', 'envioPokemon');
-    buttonSubmit.textContent = 'Enviar Pokémon!';
-    form.appendChild(buttonSubmit);
 })
 
-buttonSubmit.addEventListener('click', () => {
-    const nomePok = document.getElementById('nomePokemon');
-    const numPok = document.getElementById('numberPokemon');
-    const url = document.getElementById('imgPokemon');
+buttonSubmit.addEventListener('click', () => envioInfo());
 
-    const divItens = document.createElement('div'); // Container para cada pokémon
-    const item = document.createElement('li'); // Nome do Pokémon
-    const numItem = document.createElement('p').setAttribute('id', 'estilizarParagrafo'); // Numero do Pokémon
-    const imgItem = document.createElement('img'); // Imagem dele
-    const tipo = document.createElement('p'); // Tipo do Pokémon
+function envioInfo() {
+    if(nome.value == '' || numeroPok.value == '' || url.value == ''){
+        alert('Erro! Adicione algo aos campos!');
+        return;
+    }
 
-    item.textContent = nomePok.value;
-    numItem.textContent = numPok.value;
-    imgItem.setAttribute('src', `${url}`);
+    const Itens = document.createElement('li');
+    const paragrafo = document.createElement('p');
+    const img = document.createElement('img');
 
-    pokedexLista.appendChild(divItens);
-    divItens.appendChild(item);
-    divItens.appendChild(numItem);
-    divItens.appendChild(imgItem);
-    divItens.appendChild(tipo);
-})
+    img.setAttribute('src', url.value);
+    img.setAttribute('alt', nome.value);
+    Itens.textContent = nome.value;
+    paragrafo.textContent = numeroPok.value;
+    paragrafo.setAttribute('id', 'estilizarParagrafo');
+    
+    Itens.appendChild(paragrafo);
+    Itens.appendChild(img);
+    pokedexLista.appendChild(Itens);
+}
