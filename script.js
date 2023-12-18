45230b9bb3d3fad3a062de95b7e4b644145e730b
Thiago <thiago.goncalvesgames@gmail.com>
2023-12-18 00:02:33 -0300

    index.html    script.js    style.css

--- script.js
+++ script.js
@@ -1,34 +1,40 @@
 const pokemonButton = document.getElementById('addPokemon');
+const formulario = document.getElementById('form');
 const pokedexLista = document.getElementById('listagem');
-const form = document.querySelector('form');
 const buttonSubmit = document.getElementById('sendPokemon');
 const nome = document.getElementById('nomePokemon');
 const numeroPok = document.getElementById('numberPokemon');
 const url = document.getElementById('imgPokemon');
+const listaDeTipos = document.getElementsByClassName('tipos');
 
 pokemonButton.addEventListener('click', () => {
-    form.setAttribute('class', 'criacaoPokemon');
+    // Esconde o botão e revela o formulário
+    formulario.style.display = 'flex';
+    formulario.style.flexDirection = 'column';
+    formulario.style.alignItems = 'center'
     pokemonButton.style.display = 'none';
 })
 
 buttonSubmit.addEventListener('click', () => envioInfo());
 
 function envioInfo() {
+    // Verifica os inputs
     if(nome.value == '' || numeroPok.value == '' || url.value == ''){
         alert('Erro! Adicione algo aos campos!');
         return;
     }
 
+    // Criação dos elementos
     const Itens = document.createElement('li');
     const paragrafo = document.createElement('p');
     const img = document.createElement('img');
-
     img.setAttribute('src', url.value);
     img.setAttribute('alt', nome.value);
     Itens.textContent = nome.value;
-    paragrafo.textContent = numeroPok.value;
+    paragrafo.textContent = `Nº ${numeroPok.value}`;
     paragrafo.setAttribute('id', 'estilizarParagrafo');
     
+    // Adição deles na página
     Itens.appendChild(paragrafo);
     Itens.appendChild(img);
     pokedexLista.appendChild(Itens);
